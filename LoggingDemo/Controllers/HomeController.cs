5106fff383319b282492b3665ea9224e8bb2a959
Souvikcse <sayanwinz@gmail.com>
2021-03-05 22:28:30 +0530

    Log4net example
    Signed-off-by: Souvikcse <sayanwinz@gmail.com>

---
+++ LoggingDemo/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoggingDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ILog log = LogManager.GetLogger("mylog");
+            log.Info("Home Page request");
+            log.Error("This is an error message");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ LoggingDemo/Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace LoggingDemo
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
