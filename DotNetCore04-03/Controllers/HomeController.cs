47c22c0b1dd7f90d4ae19fb44deb8ce4fad2b49a
Souvikcse <sayanwinz@gmail.com>
2021-03-07 18:03:38 +0530

    Partial View Added
    Signed-off-by: Souvikcse <sayanwinz@gmail.com>

--- DotNetCore04-03/Controllers/HomeController.cs
+++ DotNetCore04-03/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace DotNetCore04_03.Controllers
 {
@@ -26,6 +27,41 @@ namespace DotNetCore04_03.Controllers
         public IActionResult ITOrganizations()
         {
             return View();
+        }
+        public IActionResult GetEmployeeList()
+        {
+            List<Employee> list = new List<Employee>()
+            {
+                new Employee()
+                {
+                    Id = 1,
+                    Name = "John",
+                    Salary = 10000,
+                    IsPermanent = true
+                },
+                new Employee()
+                {
+                    Id = 2,
+                    Name = "Mary",
+                    Salary = 5000,
+                    IsPermanent = false
+                },
+                new Employee()
+                {
+                    Id = 3,
+                    Name = "Smith",
+                    Salary = 5000,
+                    IsPermanent = false
+                },
+                new Employee()
+                {
+                    Id = 4,
+                    Name = "Mark",
+                    Salary = 5000,
+                    IsPermanent = false
+                }
+            };
+            return View("EmployeeList", list);
         }
         public IActionResult Privacy()
         {
---
+++ DotNetCore04-03/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCore04_03.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+        public bool IsPermanent { get; set; }
+    }
+}
