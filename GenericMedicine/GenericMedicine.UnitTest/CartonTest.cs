68c884d17fee4b5335feb082a44eeed412adfe97
Souvikcse <sayanwinz@gmail.com>
2021-03-09 00:12:16 +0530

    Stage 3 Practice Case Study
    Signed-off-by: Souvikcse <sayanwinz@gmail.com>

---
+++ GenericMedicine/GenericMedicine.UnitTest/CartonTest.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace GenericMedicine.UnitTest
+{
+    [TestFixture]
+    class CartonTest
+    {
+        Program prog;
+        Medicine medicine = new Medicine()
+        {
+            Id = 1,
+            Name = "abc",
+            GenericMedicineName = "cgf",
+            Composition = "dsb",
+            ExpiryDate = new DateTime(2025, 08, 04),
+            PricePerStrip = 8.5
+        };
+        [SetUp]
+        public void Init()
+        {
+            prog = new Program();
+        }
+
+        [Test]
+        [TestCase(10, "2022-01-09", "fdb")]
+        public void CartonObjectCreationTest(int c, DateTime date, string addr)
+        {
+
+            Assert.DoesNotThrow(() => prog.CreateCartonDetail(c, date, addr, medicine));
+        }
+
+        [Test]
+        [TestCase(-1, "2022-1-20", "fdb")]
+        public void TestStripCount(int c, DateTime date, string addr)
+        {
+            Assert.That(() => prog.CreateCartonDetail(c, date, addr, medicine), Throws.Exception);
+        }
+
+        [Test]
+        [TestCase(10, "2012-1-20", "fdb")]
+        public void LaunchDate_lessthan_CurrentDate(int c, DateTime date, string addr)
+        {
+            Assert.That(() => prog.CreateCartonDetail(c, date, addr, medicine), Throws.Exception);
+        }
+
+        [Test]
+        [TestCase(10, "2022-1-20", "fdb")]
+        public void TestNullMedicineObject(int c, DateTime date, string addr)
+        {
+            Assert.AreEqual(prog.CreateCartonDetail(c, date, addr, null), null);
+        }
+    }
+}
---
+++ GenericMedicine/GenericMedicine.UnitTest/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+
+
+namespace GenericMedicine.UnitTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        Program prog;
+        [SetUp]
+        public void Init()
+        {
+            prog = new Program();
+        }
+
+        [Test]
+        [TestCase("abc", "cdf", "fdb", 7.8)]
+        public void MedicineObjectCreationTest(string name, string medName, string comp, double p)
+        {
+            //var result = ;
+            
+
+            Assert.DoesNotThrow(() => prog.CreateMedicineDetail(name, medName, comp, DateTime.Now.AddDays(10), p));
+        }
+
+        [Test]
+        [TestCase("abc", "cdf", "fdb", 7.8)]
+        public void TestMedicineName(string name, string medName, string comp, double p)
+        {
+            Assert.That(() => prog.CreateMedicineDetail(name, null, comp, DateTime.Now.AddDays(10), p), Throws.Exception);
+        }
+
+        [Test]
+        [TestCase("abc", "cdf", "fdb", 7.8)]
+        public void TestPriceValue(string name, string medName, string comp, double p)
+        {
+            Assert.That(() => prog.CreateMedicineDetail(name, medName, comp, DateTime.Now.AddDays(10), 0), Throws.Exception);
+        }
+
+        [Test]
+        [TestCase("abc", "cdf", "fdb", 7.8)]
+        public void TestExpiryDate(string name, string medName, string comp, double p)
+        {
+            Assert.That(() => prog.CreateMedicineDetail(name, medName, comp, DateTime.Now.AddDays(-10), p), Throws.Exception);
+        }
+    }
+}
