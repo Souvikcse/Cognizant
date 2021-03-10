2f3e6df3fb0964f0386eb4868c01d01e9016b981
Souvikcse <sayanwinz@gmail.com>
2021-03-10 13:17:01 +0530

    Final Case study
    Signed-off-by: Souvikcse <sayanwinz@gmail.com>

---
+++ MyFoodSupply/MyFoodSupply.Tests/FoodItemTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyFoodSupply;
+using NUnit.Framework;
+
+namespace MyFoodSupply.Tests
+{
+    [TestFixture]
+    public class FoodItemTest
+    {
+        Program prog;
+        [SetUp]
+        public void Init()
+        {
+            prog = new Program();
+        }
+
+        [Test]
+        [TestCase("abc", 1, "2022-05-08", 8.5)]
+        public void FoodItem_Object_Creation_Test(string name, int dishType, DateTime expiryDate, double price)
+        {
+            Assert.DoesNotThrow(() => prog.CreateFoodDetail(name, dishType, expiryDate, price));
+        }
+
+        [Test]
+        [TestCase("", 1, "2022-05-08", 9.6)]
+        public void FoodItem_EmptyName_Test(string name, int dishType, DateTime expiryDate, double price)
+        {
+            Assert.Throws<Exception>(() => prog.CreateFoodDetail(name, dishType, expiryDate, price));
+        }
+
+        [Test]
+        [TestCase("nbs", 2, "2022-05-09", -8.7)]
+        public void FoodItem_NegativePrice_Test(string name, int dishType, DateTime expiryDate, double price)
+        {
+            Assert.Throws<Exception>(() => prog.CreateFoodDetail(name, dishType, expiryDate, price));
+        }
+
+        [Test]
+        [TestCase("ggf", 1, "2012-05-09", 9.5)]
+        public void FoodItem_Expirydate_Test(string name, int dishType, DateTime expiryDate, double price)
+        {
+            Assert.Throws<Exception>(() => prog.CreateFoodDetail(name, dishType, expiryDate, price));
+        }
+    }
+}
---
+++ MyFoodSupply/MyFoodSupply.Tests/SupplyDetailTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFoodSupply.Tests
+{
+    [TestFixture]
+    class SupplyDetailTest
+    {
+        Program prog;
+        [SetUp]
+        public void Init()
+        {
+            prog = new Program();
+        }
+
+        [Test]
+        [TestCase(10, "2022-03-04", "ddx", 10.2)]
+        public void SupplyDetail_Object_Creation_Test(int foodItemCount, DateTime requestDate, string sellerName, double packingCharge)
+        {
+            FoodDetail item = prog.CreateFoodDetail("abc", 1, new DateTime(2025, 01, 05), 8.5);
+            Assert.DoesNotThrow(() => prog.CreateSupplyDetail(foodItemCount, requestDate, sellerName, packingCharge, item));
+        }
+
+        [Test]
+        [TestCase(-3, "2022-03-04", "ddx", 10.2)]
+        public void SupplyDetail_itemCount_Test(int foodItemCount, DateTime requestDate, string sellerName, double packingCharge)
+        {
+            FoodDetail item = prog.CreateFoodDetail("abc", 1, new DateTime(2025, 01, 05), 8.5);
+            Assert.Throws<Exception>(() => prog.CreateSupplyDetail(foodItemCount, requestDate, sellerName, packingCharge, item));
+        }
+
+        [Test]
+        [TestCase(3, "2020-03-04", "ddx", 10.2)]
+        public void SupplyDetail_LaunchDate_Test(int foodItemCount, DateTime requestDate, string sellerName, double packingCharge)
+        {
+            FoodDetail item = prog.CreateFoodDetail("abc", 1, new DateTime(2025, 01, 05), 8.5);
+            Assert.Throws<Exception>(() => prog.CreateSupplyDetail(foodItemCount, requestDate, sellerName, packingCharge, item));
+        }
+
+    }
+}
