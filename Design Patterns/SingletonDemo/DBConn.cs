ec2aa0b36ea7abffa7c06cd31a1ab0c3740a823d
Souvikcse <sayanwinz@gmail.com>
2021-03-16 20:24:20 +0530

    Singleton
    Signed-off-by: Souvikcse <sayanwinz@gmail.com>

---
+++ Design Patterns/SingletonDemo/DBConn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    public sealed class DBConn
+    {
+        private static DBConn instance = null;
+        private static int c = 0; 
+
+        private DBConn()
+        {
+            c++;
+            Console.WriteLine("Counter Value: " + c);
+        }
+
+        public static DBConn getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new DBConn();
+            }
+            return instance;
+        }
+    }
+}
---
+++ Design Patterns/SingletonDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            DBConn obj1 = DBConn.getInstance();
+            DBConn obj2 = DBConn.getInstance();
+            Console.Read();
+        }
+    }
+}
